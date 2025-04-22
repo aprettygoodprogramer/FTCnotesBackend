9c46bbeae904459ce93c78263132f30d650a3236
Evan <evan.j.ducas@gmail.com>
2025-04-22 19:29:34 -0400

    Added the Axum Boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    http::{HeaderValue, Method},
+    routing::{get, post},
+};
+use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() {
+    let frontend_url = env::var("FRONTEND_URL").unwrap_or_else(|_| "http://localhost:5173".into());
+
+    let cors = CorsLayer::new()
+        .allow_origin(frontend_url.parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::POST, Method::GET])
+        .allow_headers(Any);
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(cors);
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
