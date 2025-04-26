d857db3639a02e0c87b8cb94a2b908d8b232560b
Evan <evan.j.ducas@gmail.com>
2025-04-26 10:07:52 -0400

    feat: implement delete event functionality and update routes

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,10 @@
 use crate::models;
-use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
+use axum::{
+    Json,
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+};
 use sqlx::{Error, Row};
 
 pub async fn get_events(State(app_state): State<models::AppState>) -> impl IntoResponse {
@@ -41,6 +46,27 @@ pub async fn create_event(
         Err(err) => {
             eprintln!("Error creating event: {:?}", err);
             (StatusCode::INTERNAL_SERVER_ERROR, "Failed to create event").into_response()
+        }
+    }
+}
+
+pub async fn delete_event(
+    State(app_state): State<models::AppState>,
+    Path(event_id): Path<i32>,
+) -> impl IntoResponse {
+    let result = sqlx::query("DELETE FROM events WHERE event_id = $1")
+        .bind(event_id)
+        .execute(&app_state.db_pool)
+        .await;
+
+    match result {
+        Ok(rows_affected) if rows_affected.rows_affected() == 1 => {
+            (StatusCode::OK, "Event deleted").into_response()
+        }
+        Ok(_) => (StatusCode::NOT_FOUND, "Event not found").into_response(),
+        Err(err) => {
+            eprintln!("Error deleting event: {:?}", err);
+            (StatusCode::INTERNAL_SERVER_ERROR, "Failed to delete event").into_response()
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,43 @@
-use std::env;
-
 use axum::{
     Json, Router,
     extract::{Path, State},
-    http::{HeaderValue, Method},
-    routing::{get, post},
+    http::{HeaderValue, Method, StatusCode},
+    response::IntoResponse,
+    routing::{delete, get, post},
 };
 use sqlx::{PgPool, postgres::PgPoolOptions};
-
-use models::AppState;
 use tokio::net::TcpListener;
 use tower_http::cors::{Any, CorsLayer};
+
 mod handlers;
 mod models;
+use models::AppState;
+
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
-    let frontend_url = env::var("FRONTEND_URL").unwrap_or_else(|_| "http://localhost:5173".into());
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let frontend_url =
+        std::env::var("FRONTEND_URL").unwrap_or_else(|_| "http://localhost:5173".into());
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
     let cors = CorsLayer::new()
         .allow_origin(frontend_url.parse::<HeaderValue>().unwrap())
-        .allow_methods([Method::POST, Method::GET])
+        .allow_methods([Method::POST, Method::GET, Method::DELETE])
         .allow_headers(Any);
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
         .await
         .expect("Failed to create pool");
 
-    let app_state: AppState = AppState { db_pool: pool };
+    let app_state = AppState { db_pool: pool };
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/events", get(handlers::get_events))
         .route("/create-event", post(handlers::create_event))
+        .route("/delete-event/{event_id}", delete(handlers::delete_event))
         .layer(cors)
         .with_state(app_state);
 
